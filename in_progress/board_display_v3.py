916986d1de7b9a71d02cfdb24144d51797beae23
Thaddeus  Thomas <92204097+thomasthaddeus@users.noreply.github.com>
2022-02-16 13:10:33 -0700

    Update and rename Untitled-1.py to board_display_v3.py

--- in_progress/board_display_v3.py
+++ in_progress/board_display_v3.py
@@ -1,13 +1,6 @@
 def draw():
     # initialize an empty board
-    board = '''
-     {} | {} | {} 
-    ____|____|____
-        |    |    
-     {} | {} | {} 
-    ____|____|____
-        |    |    
-     {} | {} | {} ''' 
+    board = '' 
 
     # there are 5 rows in a standard tic-tac-toe board
     for i in range(5):
