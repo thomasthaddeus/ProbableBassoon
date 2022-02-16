f8ed54976d22fe689184e7518acbe20aa3b948e3
Thaddeus  Thomas <92204097+thomasthaddeus@users.noreply.github.com>
2022-02-16 03:25:57 -0700

    syncing

--- in_progress/Untitled-1.py
+++ in_progress/Untitled-1.py
@@ -1,6 +1,13 @@
 def draw():
     # initialize an empty board
-    board = ""
+    board = '''
+     {} | {} | {} 
+    ____|____|____
+        |    |    
+     {} | {} | {} 
+    ____|____|____
+        |    |    
+     {} | {} | {} ''' 
 
     # there are 5 rows in a standard tic-tac-toe board
     for i in range(5):
